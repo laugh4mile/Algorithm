6dbcadcfd0694f5b0bf49b5c2d118faf640f8a2d
yhs <laugh4mile>
2020-11-05 19:08:37 +0900

    정리

---
+++ github/src/com/base/goodmorning.java
@@ -0,0 +1,66 @@
+package com.base;
+
+import java.util.Arrays;
+
+public class goodmorning {
+	static int N,R;
+	static int num[];
+	static int result[];
+	static boolean isSelected[];
+	public static void main(String[] args) {
+		N = 4;
+		R = 2;
+		num = new int [N];
+		result = new int[R];
+		isSelected = new boolean[N];
+		
+		for(int i=0; i<N; i++) {
+			num[i] = i+1;
+		}
+		
+//		permutation(0);
+//		combination(0,0);
+		subSet(0);
+		
+	}
+	private static void subSet(int cnt) {
+		if(cnt == N) {
+			for(int i=0; i<N; i++) {
+				if(isSelected[i]) {
+					System.out.print(num[i]);
+				}
+			}
+			System.out.println();
+			return;
+		}
+		isSelected[cnt] = true;
+		subSet(cnt+1);
+		isSelected[cnt] = false;
+		subSet(cnt+1);
+	}
+	private static void combination(int start, int cnt) {
+		if(cnt == R) {
+			System.out.println(Arrays.toString(result));
+			return;
+		}
+		for(int i = start; i<N; i++) {
+			result[cnt] = num[i];
+			combination(i+1, cnt+1);
+		}
+	}
+	private static void permutation(int cnt) {
+		if(cnt == R) {
+			System.out.println(Arrays.toString(result));
+			return;
+		}
+		for(int i=0; i<N; i++) {
+			if(isSelected[i]) continue;
+			
+			result[cnt] = num[i];
+			isSelected[i] = true;
+			permutation(cnt+1);
+			isSelected[i] = false;
+		}
+	}
+
+}
