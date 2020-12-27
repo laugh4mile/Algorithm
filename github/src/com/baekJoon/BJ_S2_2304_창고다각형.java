99b5ac6e28aff370f6b0d11fdfea4c98daedfec2
yhs <laugh4mile@naver.com>
2020-12-28 00:18:49 +0900

    BJ_S2_2304_창고다각형.java

--- github/src/com/baekJoon/BJ_S2_2304_창고다각형.java
+++ github/src/com/baekJoon/BJ_S2_2304_창고다각형.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -14,47 +13,66 @@ public class BJ_S2_2304_창고다각형 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer tokens;
 	static int N;
-	static List<column> list = new ArrayList<>();
+	static List<Column> list = new ArrayList<>();
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		input = new BufferedReader(new StringReader(src));
 		N = Integer.parseInt(input.readLine());
+		int maxH = 0;
+		int maxL = 0;
 		for(int i=0; i<N; i++) {
 			tokens = new StringTokenizer(input.readLine());
 			int l = Integer.parseInt(tokens.nextToken());
 			int h = Integer.parseInt(tokens.nextToken());
-			list.add(new column(l, h));
-		}
-		Collections.sort(list, new Comparator<column>() {
-			@Override
-			public int compare(column o1, column o2) {
-				
-				return Integer.compare(o1.L, o2.L);
+			list.add(new Column(l, h));
+			if(h > maxH) {
+				maxH = h;
 			}
-		});
-		System.out.println(list);
-		
-		for(int i=0; i<N; i++) {
-			for(int j=i; j<N; j++) {
-				if(list.get(i).H > list.get(j).H) {
-					
+		}
+		Collections.sort(list);
+		for(int i=0; i<list.size(); i++) {
+			if(list.get(i).H == maxH) {
+				maxL = i;
+				break;
+			}
+		}
+//		System.out.println(maxL);
+		int answer = 0;
+		for(int i=0; i<maxL; i++) {
+			for(int j=i+1; j<=maxL; j++) {
+				if(list.get(i).H <= list.get(j).H) {
+					answer += (list.get(j).L - list.get(i).L) * list.get(i).H;
+					i = j;
 				}
 			}
 		}
+		
+		for(int i=N-1; i>maxL; i--) {
+			for(int j=i-1; j>=maxL; j--) {
+				if(list.get(i).H <= list.get(j).H) {
+					answer += (list.get(i).L - list.get(j).L) * list.get(i).H;
+					i = j;
+				}
+			}
+		}
+		System.out.println(answer+maxH);
 	}
 	
-	static class column{
+	static class Column implements Comparable<Column>{
 		int L;
 		int H;
-		public column(int l, int h) {
+		public Column(int l, int h) {
 			super();
 			L = l;
 			H = h;
 		}
 		@Override
 		public String toString() {
-			return "column [L=" + L + ", H=" + H + "]";
+			return "column [L=" + L + ", H=" + H + "]\n";
 		}
-		
+		@Override
+		public int compareTo(Column o) {
+			return Integer.compare(this.L, o.L);
+		}
 		
 	}
 	
