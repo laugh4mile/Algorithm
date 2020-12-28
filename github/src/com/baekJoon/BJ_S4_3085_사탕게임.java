374428bebcfee4ca8aa36ce33cd82f204000fd69
yhs <laugh4mile@naver.com>
2020-12-28 22:43:38 +0900

    BJ_S4_3085_사탕게임.java

--- github/src/com/baekJoon/BJ_S4_3085_사탕게임.java
+++ github/src/com/baekJoon/BJ_S4_3085_사탕게임.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.util.Arrays;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class BJ_S4_3085_사탕게임 {
@@ -14,7 +12,7 @@ public class BJ_S4_3085_사탕게임 {
 	static int N, max=1;
 	static char map[][];
 	public static void main(String[] args) throws NumberFormatException, IOException {
-//		input = new BufferedReader(new StringReader(src));
+		input = new BufferedReader(new StringReader(src));
 		N = Integer.parseInt(input.readLine());
 		map = new char[N][N];
 		for(int r=0; r<N; r++) {
@@ -23,9 +21,6 @@ public class BJ_S4_3085_사탕게임 {
 				map[r][c] = line.charAt(c);
 			}	
 		}
-//		for(char x[] : map) {
-//			System.out.println(Arrays.toString(x));
-//		}
 		
 		solve();
 		System.out.println(max);
@@ -61,52 +56,46 @@ public class BJ_S4_3085_사탕게임 {
 	}
 
 	private static void getMaxCandy() {
-		Stack<Character> stack = new Stack<>();
+		char peek = '0';
 		for(int r=0; r<N; r++) {
 			int cnt = 0;
 			for(int c=0; c<N; c++) {
-				if(stack.isEmpty()) {
-					stack.add(map[r][c]);
+				if(peek == '0') {
+					peek = map[r][c];
 					cnt = 1;
 				}else {
-					if(stack.peek() == map[r][c]) {
+					if(peek == map[r][c]) {
 						cnt++;
-						stack.add(map[r][c]);
 						if(max < cnt) {
 							max = cnt;
 						}
 					}else {
 						cnt = 1;
-						stack.add(map[r][c]);
+						peek = map[r][c];
 					}
 				}
-				stack.add(map[r][c]);
 			}
 		}
-		stack.clear();
+		peek = '0';
 		for(int c=0; c<N; c++) {
-//			Stack<Character> stack = new Stack<>();
 			int cnt = 0;
 			for(int r=0; r<N; r++) {
-				if(stack.isEmpty()) {
-					stack.add(map[r][c]);
+				if(peek == '0') {
+					peek = map[r][c];
 					cnt = 1;
 				}else {
-					if(stack.peek() == map[r][c]) {
+					if(peek == map[r][c]) {
 						cnt++;
-						stack.add(map[r][c]);
 						if(max < cnt) {
 							max = cnt;
 						}
 					}else {
 						cnt = 1;
-						stack.add(map[r][c]);
+						peek = map[r][c];
 					}
 				}
-				stack.add(map[r][c]);
 			}
 		}
-		stack.clear();
 	}
 
 	static boolean isIn(int r, int c) {
