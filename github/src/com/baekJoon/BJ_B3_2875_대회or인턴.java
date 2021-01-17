e43bd714ce39fae19a1467395da5ae68ae83ceb3
yhs <laugh4mile@naver.com>
2021-01-17 23:36:51 +0900

    BJ_B3_2875_대회or인턴.java

---
+++ github/src/com/baekJoon/BJ_B3_2875_대회or인턴.java
@@ -0,0 +1,39 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class BJ_B3_2875_대회or인턴 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+	static int N,M,K;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		input = new BufferedReader(new StringReader(src));
+		tokens = new StringTokenizer(input.readLine());
+		N = Integer.parseInt(tokens.nextToken());
+		M = Integer.parseInt(tokens.nextToken());
+		K = Integer.parseInt(tokens.nextToken());
+		
+		while(K!=0) {
+			if(N/2 >= M) {
+				N--;
+				K--;
+			}else {
+				M--;
+				K--;
+			}
+		}
+//		System.out.println(N +" "+M);
+		if(N/2 >= M) {
+			System.out.println(M);
+		}else {
+			System.out.println(N/2);
+		}
+	}
+
+	static String src =
+			"2 5 3";
+}
