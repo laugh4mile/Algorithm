57289469bc907c22f6b79546e5be9725146e732d
laugh4mile <laugh4mile@naver.com>
2021-04-04 00:01:20 +0900

    데브매칭 코딩테스트

---
+++ github/src/com/cordingtest/devmatching/S2.java
@@ -0,0 +1,16 @@
+package com.cordingtest.devmatching;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class S2 {
+	
+	public static int[] solution(int rows, int columns, int[] queries) {
+        int[] answer = new int[] {};
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+//		solution(rows, columns)
+	}
+}
