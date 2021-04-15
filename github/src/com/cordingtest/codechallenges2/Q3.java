f544d5c2ee6a757f09e3054ff30d0b8e9572126d
Hyunsoo <laugh4mile@naver.com>
2021-04-15 20:47:48 +0900

    0415 코드챌린지

---
+++ github/src/com/cordingtest/codechallenges2/Q3.java
@@ -0,0 +1,23 @@
+package com.cordingtest.codechallenges2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.Stack;
+
+public class Q3 {
+	
+	static int a[] = {-5,0,2,1,2};
+	static int edges[][] = {{0,1},{3,4},{2,3},{0,3}};
+	
+	public static long solution(int[] a, int[][] edges) {
+        long answer = -2;
+        return answer;
+    }
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		solution(a, edges);
+	}
+	//포기
+}
