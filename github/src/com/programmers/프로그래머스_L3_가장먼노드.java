980c117487b717dcfc4dd0f9d38021a4bc98789b
Hyunsoo <laugh4mile@naver.com>
2021-05-01 00:29:32 +0900

    프로그래머스_L3_가장먼노드

--- github/src/com/programmers/프로그래머스_L3_가장먼노드.java
+++ github/src/com/programmers/프로그래머스_L3_가장먼노드.java
@@ -3,6 +3,11 @@ package com.programmers;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 /**
  * @author yhs
@@ -23,17 +28,61 @@ import java.io.InputStreamReader;
 
 public class 프로그래머스_L3_가장먼노드 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static int n = 6;
+	static int n = 6, depth=0;
 	static int [][] edge = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
-	
-	
+	static int distance[];
+	static List<Integer> graph[];
+	static boolean isVisited[];
     public static int solution(int n, int[][] edge) {
         int answer = 0;
+        isVisited = new boolean[n+1];
+        distance = new int[n+1];
+        graph = new ArrayList[n+1];
+        for(int i=1; i<n+1; i++) {
+        	graph[i] = new ArrayList<>();
+        }
         
+        for(int i=0; i<edge.length; i++) {
+        	graph[edge[i][0]].add(edge[i][1]);
+        	graph[edge[i][1]].add(edge[i][0]);
+        }
+//        for(List<Integer> x : graph) {
+//        	System.out.println(x);
+//        }
+        
+        bfs(1);
+        
+        Arrays.sort(distance);
+//        System.out.println(Arrays.toString(distance));
+        int max = distance[n];
+        for(int i=0; i<= n; i++) {
+        	if(distance[i] == max) {
+        		answer++;
+        	}
+        }
         return answer;
     }
     
+	private static void bfs(int start) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.offer(start);
+		isVisited[start] = true;
+		
+		while(!queue.isEmpty()) {
+			Integer front = queue.poll();
+			List<Integer> childs = graph[front];
+			for(int i=0; i<childs.size(); i++) {
+				Integer child = childs.get(i);
+				if(!isVisited[child]) {
+					queue.offer(child);
+					isVisited[child] = true;
+					distance[child] = distance[front]+1;
+				}
+			}
+		}
+	}
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		solution(n, edge);
+		System.out.println(solution(n, edge));
 	}
 }
