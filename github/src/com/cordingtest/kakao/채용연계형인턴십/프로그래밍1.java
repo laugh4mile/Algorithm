9c5cc588d612c38f01153d577f62a85a6fda7e10
Hyunsoo <laugh4mile@naver.com>
2021-05-08 21:42:35 +0900

    kakao 채용연계형인턴십 코딩테스트

---
+++ github/src/com/cordingtest/kakao/채용연계형인턴십/프로그래밍1.java
@@ -0,0 +1,21 @@
+package com.cordingtest.kakao.채용연계형인턴십;
+
+public class 프로그래밍1 {
+	public int solution(String s) {
+        int answer = 0;
+        s = s.replace("zero","0");
+        s = s.replace("one","1");
+        s = s.replace("two","2");
+        s = s.replace("three","3");
+        s = s.replace("four","4");
+        s = s.replace("five","5");
+        s = s.replace("six","6");
+        s = s.replace("seven","7");
+        s = s.replace("eight","8");
+        s = s.replace("nine","9");
+        
+        answer = Integer.parseInt(s);
+        
+        return answer;
+    }
+}
