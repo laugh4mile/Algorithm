55027c2220fdbc109afdb3382d818a5edfde5a14
Hyunsoo <laugh4mile@naver.com>
2021-05-10 23:51:26 +0900

    프로그래머스_L2_전화번호목록

---
+++ github/src/com/programmers/프로그래머스_L2_전화번호목록.java
@@ -0,0 +1,18 @@
+package com.programmers;
+
+import java.util.Arrays;
+
+public class 프로그래머스_L2_전화번호목록 {
+	public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        
+        for(int i=0; i<phone_book.length-1; i++){
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                return false;
+            }
+        }
+        
+        return answer;
+    }
+}
