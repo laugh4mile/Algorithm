903adf394aa6fbe735d9b51f9238e1ec77220053
Hyunsoo <laugh4mile@naver.com>
2021-05-25 23:42:55 +0900

    BJ_G5_12886_돌그룹2

--- github/src/com/baekJoon/BJ_G5_12886_돌그룹2.java
+++ github/src/com/baekJoon/BJ_G5_12886_돌그룹2.java
@@ -22,15 +22,10 @@ public class BJ_G5_12886_돌그룹2 {
 			sum += stones[i];
 		}
 		Arrays.sort(stones);
-		
 		dfs(stones);
-		
 		System.out.println(answer);
 	}
 	
-
-
-
 	private static void dfs(int[] stones) {
 		if(stones[0] == stones[1] && stones[1] == stones[2]) {
 			answer = true;
@@ -42,13 +37,18 @@ public class BJ_G5_12886_돌그룹2 {
 		isVisited[stones[1]][stones[2]] = true;
 		isVisited[stones[2]][stones[1]] = true;
 		
+		if(stones[0]!=stones[1]) {
+			int temp = stones[0];
+			stones[0] = temp * 2;
+			stones[1] = stones[1] - temp;
+			Arrays.sort(stones);
+			if(!isVisited[stones[0]][stones[1]]) {
+				dfs(stones);
+			}
+		}
 	}
 
-
-
-
 	static String src =
-
 			"1 1 2";
 }
 
