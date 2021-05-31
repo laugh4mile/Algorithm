84ebdd4f51e617e050b3f942a892d0b2672d9bf5
Hyunsoo <laugh4mile@naver.com>
2021-06-01 00:27:15 +0900

    BJ_G5_7662_우선순위큐2.java

--- github/src/com/baekJoon/BJ_G5_7662_우선순위큐2.java
+++ github/src/com/baekJoon/BJ_G5_7662_우선순위큐2.java
@@ -15,7 +15,7 @@ public class BJ_G5_7662_우선순위큐2 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		input = new BufferedReader(new StringReader(src));
 		T = Integer.parseInt(input.readLine());
-		
+
 		for (int t = 0; t < T; t++) {
 			TreeMap<Integer, Integer> map = new TreeMap<>();
 			K = Integer.parseInt(input.readLine());
@@ -24,31 +24,55 @@ public class BJ_G5_7662_우선순위큐2 {
 				char cmd = tokens.nextToken().charAt(0);
 				int num = Integer.parseInt(tokens.nextToken());
 				if (cmd == 'I') {
-					map.put(num, num);
+					map.put(num, map.getOrDefault(num, 0) + 1);
 				}
 				if (cmd == 'D') {
-					if(num==-1) {
-						if(map.size()>0) {
-							map.remove(map.firstKey());
+					if(map.isEmpty()) {
+						continue;
+					}
+					if (num == -1) {
+						int min = map.firstKey();
+						if (map.get(min) == 1) {
+							map.remove(min);
+						} else {
+							map.replace(min, map.get(min)- 1);
 						}
-					}else {
-						if(map.size()>0) {
-							map.remove(map.lastKey());
+					} else {
+						int max = map.lastKey();
+						if (map.get(max) == 1) {
+							map.remove(max);
+						} else {
+							map.replace(max, map.get(max)- 1);
 						}
-
 					}
 				}
 			}
-			if(map.size()<=0) {
+			if (map.isEmpty()) {
 				System.out.println("EMPTY");
-			}else {
-				
-				System.out.println(map.get(map.lastKey())+" "+map.firstKey());
+			} else {
+				System.out.println(map.lastKey() + " " + map.firstKey());
 			}
 		}
 	}
 
-	static String src = "2\r\n" + "7\r\n" + "I 16\r\n" + "I -5643\r\n" + "D -1\r\n" + "D 1\r\n" + "D 1\r\n"
-			+ "I 123\r\n" + "D -1\r\n" + "9\r\n" + "I -45\r\n" + "I 653\r\n" + "D 1\r\n" + "I -642\r\n" + "I 45\r\n"
-			+ "I 97\r\n" + "D 1\r\n" + "D -1\r\n" + "I 333";
+	static String src = 
+			"2\r\n" + 
+			"7\r\n" + 
+			"I 16\r\n" + 
+			"I -5643\r\n" + 
+			"D -1\r\n" + 
+			"D 1\r\n" + 
+			"D 1\r\n" + 
+			"I 123\r\n" + 
+			"D -1\r\n" + 
+			"9\r\n" + 
+			"I -45\r\n" + 
+			"I 653\r\n" + 
+			"D 1\r\n" + 
+			"I -642\r\n" + 
+			"I 45\r\n" + 
+			"I 97\r\n" + 
+			"D 1\r\n" + 
+			"D -1\r\n" + 
+			"I 333";
 }
