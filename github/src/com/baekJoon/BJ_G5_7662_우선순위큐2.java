71534a2d79e576f3fbaac445943020327122f578
Hyunsoo <laugh4mile@naver.com>
2021-05-31 23:30:13 +0900

    BJ_G5_7662_우선순위큐2

--- github/src/com/baekJoon/BJ_G5_7662_우선순위큐2.java
+++ github/src/com/baekJoon/BJ_G5_7662_우선순위큐2.java
@@ -1,53 +1,51 @@
 package com.baekJoon;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.StringReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
-public class BJ_G5_7662_우선순위큐 {
+public class BJ_G5_7662_우선순위큐2 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer tokens;
 	static int T, K;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		input = new BufferedReader(new StringReader(src));
 		T = Integer.parseInt(input.readLine());
+		
 		for (int t = 0; t < T; t++) {
-			PriorityQueue<Integer> pq = new PriorityQueue<>();
-			PriorityQueue<Integer> pq2 = new PriorityQueue<>(Collections.reverseOrder());
+			TreeMap<Integer, Integer> map = new TreeMap<>();
 			K = Integer.parseInt(input.readLine());
 			for (int k = 0; k < K; k++) {
 				tokens = new StringTokenizer(input.readLine());
 				char cmd = tokens.nextToken().charAt(0);
 				int num = Integer.parseInt(tokens.nextToken());
 				if (cmd == 'I') {
-					pq.offer(num);
-					pq2.offer(num);
+					map.put(num, num);
 				}
 				if (cmd == 'D') {
-					if (num == -1 && !pq.isEmpty()) {
-						int min = pq.poll();
-						pq2.remove(min);
-					} else if (num == 1 && !pq2.isEmpty()) {
-						int max = pq2.poll();
-						pq.remove(max);
+					if(num==-1) {
+						if(map.size()>0) {
+							map.remove(map.firstKey());
+						}
+					}else {
+						if(map.size()>0) {
+							map.remove(map.lastKey());
+						}
+
 					}
 				}
 			}
-			if(pq.isEmpty()) {
-				output.append("EMPTY\n");
+			if(map.size()<=0) {
+				System.out.println("EMPTY");
 			}else {
-				output.append(pq.peek()+" "+pq2.peek()+"\n");
+				
+				System.out.println(map.get(map.lastKey())+" "+map.firstKey());
 			}
 		}
-		output.close();
 	}
 
 	static String src = "2\r\n" + "7\r\n" + "I 16\r\n" + "I -5643\r\n" + "D -1\r\n" + "D 1\r\n" + "D 1\r\n"
