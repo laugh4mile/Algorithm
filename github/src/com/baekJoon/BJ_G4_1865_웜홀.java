02c5f905f1aa3b18645d5b116227e232bf489671
Hyunsoo <laugh4mile@naver.com>
2021-06-14 23:46:59 +0900

    BJ_G4_1865_웜홀

---
+++ github/src/com/baekJoon/BJ_G4_1865_웜홀.java
@@ -0,0 +1,30 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class BJ_G4_1865_웜홀 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		input = new BufferedReader(new StringReader(src));
+		
+	}
+
+	static String src =
+
+			"2\r\n" + 
+			"3 3 1\r\n" + 
+			"1 2 2\r\n" + 
+			"1 3 4\r\n" + 
+			"2 3 1\r\n" + 
+			"3 1 3\r\n" + 
+			"3 2 1\r\n" + 
+			"1 2 3\r\n" + 
+			"2 3 4\r\n" + 
+			"3 1 8";
+}
