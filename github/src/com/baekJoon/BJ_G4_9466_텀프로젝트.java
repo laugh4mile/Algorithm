76a48723a4f2e0778fe284d7d83389a101a5fdd5
Hyunsoo <laugh4mile@naver.com>
2021-06-24 23:38:25 +0900

    BJ_G4_9466_텀프로젝트

---
+++ github/src/com/baekJoon/BJ_G4_9466_텀프로젝트.java
@@ -0,0 +1,24 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class BJ_G4_9466_텀프로젝트 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		input = new BufferedReader(new StringReader(src));
+		
+	}
+
+	static String src =
+			"2\r\n" + 
+			"7\r\n" + 
+			"3 1 3 7 3 4 6\r\n" + 
+			"8\r\n" + 
+			"1 2 3 4 5 6 7 8";
+}
