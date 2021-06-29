62c914d8fffc2eb7ade77c54acbb8dd8b7c02fef
Hyunsoo <laugh4mile@naver.com>
2021-06-29 22:22:14 +0900

    BJ_S1_14496_그대그머가되어

---
+++ github/src/com/baekJoon/BJ_S1_14496_그대그머가되어.java
@@ -0,0 +1,25 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class BJ_S1_14496_그대그머가되어 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		input = new BufferedReader(new StringReader(src));
+		
+	}
+
+	static String src = 
+			"1 2\r\n"
+			+ "4 4\r\n"
+			+ "1 3\r\n"
+			+ "1 4\r\n"
+			+ "3 2\r\n"
+			+ "4 2";
+}
