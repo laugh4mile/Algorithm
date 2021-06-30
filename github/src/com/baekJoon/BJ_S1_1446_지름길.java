22b2fc3f73352621367d6780f83a43acc80618b4
Hyunsoo <laugh4mile@naver.com>
2021-06-30 22:03:31 +0900

    BJ_S1_1446_지름길

---
+++ github/src/com/baekJoon/BJ_S1_1446_지름길.java
@@ -0,0 +1,26 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class BJ_S1_1446_지름길 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		input = new BufferedReader(new StringReader(src));
+		
+	}
+
+	static String src =
+
+			"5 150\r\n" + 
+			"0 50 10\r\n" + 
+			"0 50 20\r\n" + 
+			"50 100 10\r\n" + 
+			"100 151 10\r\n" + 
+			"110 140 90";
+}
