fa3deffca671791c9f7d700fa52ea2744b9e746d
Hyunsoo <laugh4mile@naver.com>
2021-07-13 01:04:26 +0900

    BJ_S2_5464_주차장

---
+++ github/src/com/baekJoon/BJ_S2_5464_주차장.java
@@ -0,0 +1,35 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class BJ_S2_5464_주차장 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		input = new BufferedReader(new StringReader(src));
+		
+	}
+
+	static String src =
+			"3 4\r\n" + 
+			"2\r\n" + 
+			"3\r\n" + 
+			"5\r\n" + 
+			"200\r\n" + 
+			"100\r\n" + 
+			"300\r\n" + 
+			"800\r\n" + 
+			"3\r\n" + 
+			"2\r\n" + 
+			"-3\r\n" + 
+			"1\r\n" + 
+			"4\r\n" + 
+			"-4\r\n" + 
+			"-2\r\n" + 
+			"-1";
+}
