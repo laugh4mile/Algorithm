a1243bb57eb3b4d2560bd3569ad8a6ae8f8e3ee8
Hyunsoo <laugh4mile@naver.com>
2021-07-19 00:23:00 +0900

    BJ_S1_20923_숫자할리갈리게임

--- github/src/com/baekJoon/BJ_S1_20923_숫자할리갈리게임.java
+++ github/src/com/baekJoon/BJ_S1_20923_숫자할리갈리게임.java
@@ -32,43 +32,47 @@ public class BJ_S1_20923_숫자할리갈리게임 {
 			suyun.offerFirst(s);
 		}
 		System.out.println(getWinner(dodo,suyun,gD,gS,M));
-		
 	}
 
 	private static String getWinner(Deque<Integer> dodo, Deque<Integer> suyun, Deque<Integer> gD, Deque<Integer> gS, int M) {
 		while(M>0) {
-			System.out.println(dodo);
-			System.out.println(suyun);
-			System.out.println();
-//			System.out.println(gD);
-//			System.out.println(gS);
-//			System.out.println();
 			// 1. 도도 턴
 			if(!dodo.isEmpty()) {
 				gD.offerFirst(dodo.pollFirst());
+				M--;
+				if(dodo.isEmpty()) {
+					return "su";
+				}
 			}else { // 도도 패 다 털렸네?
 				return "su";
 			}
+			
 			// 2. 종 칠지 확인
 			if(!gD.isEmpty() && !gS.isEmpty() && gD.peek() + gS.peek() == 5) { // 수연이가 종을 치는 경우
 				while(!gD.isEmpty()) {
-					suyun.offerLast(gD.poll());
+					suyun.offerLast(gD.pollLast());
 				}
 				while(!gS.isEmpty()) {
-					suyun.offerLast(gS.poll());
+					suyun.offerLast(gS.pollLast());
 				}
 			}else if((!gD.isEmpty() && gD.peek() == 5) || (!gS.isEmpty() && gS.peek() == 5)) { // 도도가 종을 치는 경우
 				while(!gS.isEmpty()) {
-					dodo.offerLast(gS.poll());
+					dodo.offerLast(gS.pollLast());
 				}
 				while(!gD.isEmpty()) {
-					dodo.offerLast(gD.poll());
+					dodo.offerLast(gD.pollLast());
 				}
 			}
-			
+			if(M<=0) {
+				break;
+			}
 			// 3. 수연 턴
 			if(!suyun.isEmpty()) {
+				M--;
 				gS.offerFirst(suyun.pollFirst());
+				if(suyun.isEmpty()) {
+					return "do";
+				}
 			}else { // 수연이 패 다 털렸네?
 				return "do";
 			}
@@ -76,20 +80,19 @@ public class BJ_S1_20923_숫자할리갈리게임 {
 			// 4. 종 칠지 확인
 			if(!gD.isEmpty() && !gS.isEmpty() && gD.peek() + gS.peek() == 5) { // 수연이가 종을 치는 경우
 				while(!gD.isEmpty()) {
-					suyun.offerLast(gD.poll());
+					suyun.offerLast(gD.pollLast());
 				}
 				while(!gS.isEmpty()) {
-					suyun.offerLast(gS.poll());
+					suyun.offerLast(gS.pollLast());
 				}
 			}else if((!gD.isEmpty() && gD.peek() == 5) || (!gS.isEmpty() && gS.peek() == 5)) { // 도도가 종을 치는 경우
 				while(!gS.isEmpty()) {
-					dodo.offerLast(gS.poll());
+					dodo.offerLast(gS.pollLast());
 				}
 				while(!gD.isEmpty()) {
-					dodo.offerLast(gD.poll());
+					dodo.offerLast(gD.pollLast());
 				}
 			}
-			M--;
 		}
 		if(dodo.size() > suyun.size()) {
 			return "do";
