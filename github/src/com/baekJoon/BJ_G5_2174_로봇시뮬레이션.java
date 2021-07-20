b011ca96a3aed3fd628d9fc98a43d27264064690
Hyunsoo <laugh4mile@naver.com>
2021-07-20 17:15:00 +0900

    BJ_G5_2174_로봇시뮬레이션

--- github/src/com/baekJoon/BJ_G5_2174_로봇시뮬레이션.java
+++ github/src/com/baekJoon/BJ_G5_2174_로봇시뮬레이션.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
