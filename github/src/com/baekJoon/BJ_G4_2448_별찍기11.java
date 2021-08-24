60a1b325b9626da4559d8de25d4bc64007874fb9
Hyunsoo <laugh4mile@naver.com>
2021-08-25 01:28:19 +0900

    BJ_G4_2448_별찍기11 char[] 는 공백 판정이 아니다

--- github/src/com/baekJoon/BJ_G4_2448_별찍기11.java
+++ github/src/com/baekJoon/BJ_G4_2448_별찍기11.java
@@ -8,7 +8,7 @@ import java.io.OutputStreamWriter;
 import java.io.StringReader;
 import java.util.StringTokenizer;
 
-public class BJ_S1_2449_별찍기10 {
+public class BJ_G4_2448_별찍기11 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer tokens;
@@ -16,40 +16,50 @@ public class BJ_S1_2449_별찍기10 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		input = new BufferedReader(new StringReader(src));
 		int N = Integer.parseInt(input.readLine());
-		char map[][] = new char[N][N];
+		char map[][] = new char[N][2*N-1];
+		int cnt;
 		for(int r=0; r<N; r++) {
-			for(int c=0; c<N; c++) {
+			cnt = N-1-r;
+			for(int c=cnt; c<cnt+4*r+1; c++) {
 				map[r][c] = '*';
-			}	
+				cnt--;
+			}
 		}
-		makeHole(0, 0, N, map);
+		makeHole(0,0,N,map);
 		for(int r=0; r<N; r++) {
-			for(int c=0; c<N; c++) {
-				output.append(map[r][c]);
-			}	
+			for(int c=0; c<2*N-1; c++) {
+				if(map[r][c] == '*') {
+					output.append(map[r][c]);
+				}else {
+					output.append(" ");
+				}
+			}
 			output.append("\n");
 		}
 		output.close();
 	}
 
+
 	private static void makeHole(int nr, int nc, int n, char[][] map) {
-		if(n<3) {
+		if(n==3) {
+			map[nr+1][nc+2] = ' ';
 			return;
 		}
-		for(int r=nr+n/3; r<nr+n/3*2; r++) {
-			for(int c=nc+n/3; c<nc+n/3*2; c++) {
-				map[r][c] = ' ';
-			}
-		}
+		
 		int cnt = 0;
-		for(int r=nr; r<nr+n; r+=n/3) {
-			for(int c=nc; c<nc+n; c+=n/3) {
-				if(++cnt == 5) continue;
-				makeHole(r, c, n/3, map);
+		for(int r=nr+n/2; r<nr+n; r++) {
+			for(int c=nc+n/2+cnt; c<nc+n/2*3-1-cnt; c++) {
+				map[r][c] = ' ';
 			}	
+			cnt++;
 		}
+		makeHole(nr, nc+n/2, n/2, map);
+		makeHole(nr+n/2, nc, n/2, map);
+		makeHole(nr+n/2, nc+n, n/2, map);
+		
 	}
 
+
 	static String src =
-			"27";
+			"3";
 }
