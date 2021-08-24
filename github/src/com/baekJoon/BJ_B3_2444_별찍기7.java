bd4be401aff263cf339411550083106c5ef5e140
Hyunsoo <laugh4mile@naver.com>
2021-08-24 23:54:56 +0900

    별찍기 마스터중

---
+++ github/src/com/baekJoon/BJ_B3_2444_별찍기7.java
@@ -0,0 +1,42 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class BJ_B3_2444_별찍기7 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer tokens;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		input = new BufferedReader(new StringReader(src));
+		int N = Integer.parseInt(input.readLine());
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N-i-1; j++) {
+				output.append(" ");
+			}
+			for(int j=0; j<i*2+1; j++) {
+				output.append("*");
+			}
+			output.append("\n");
+		}
+		for(int i=1; i<N; i++) {
+			for(int j=0; j<i; j++) {
+				output.append(" ");
+			}
+			for(int j=i; j<N*2-1-i; j++) {
+				output.append("*");
+			}
+			output.append("\n");
+		}
+		output.close();
+	}
+
+	static String src =
+			"5";
+}
---
+++ github/src/com/baekJoon/BJ_S1_2449_별찍기10.java
@@ -0,0 +1,26 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class BJ_S1_2449_별찍기10 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer tokens;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		input = new BufferedReader(new StringReader(src));
+		int N = Integer.parseInt(input.readLine());
+		for(int i=0; i<N; i++) {
+			
+		}
+	}
+
+	static String src =
+			"27";
+}
