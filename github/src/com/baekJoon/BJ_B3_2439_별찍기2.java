2a6293917ca97a0ebdd5abc590f5d41b18a0e0c0
Hyunsoo <laugh4mile@naver.com>
2021-08-24 23:10:44 +0900

    BJ_B3_2439_별찍기2

---
+++ github/src/com/baekJoon/BJ_B3_2439_별찍기2.java
@@ -0,0 +1,31 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+
+public class BJ_B3_2439_별찍기2 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		input = new BufferedReader(new StringReader(src));
+		int N = Integer.parseInt(input.readLine());
+		for(int i=0; i<N; i++) {
+			for(int j=N-1; j>i; j--) {
+				output.append(' ');
+			}
+			for(int j=0; j<=i; j++) {
+				output.append('*');
+			}
+			output.append("\n");
+		}
+		output.close();
+	}
+
+	static String src =
+			"5";
+}
