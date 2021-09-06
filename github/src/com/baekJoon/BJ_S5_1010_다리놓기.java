58b0cfade81017d74895ef4a8633a86852de4873
Hyunsoo <laugh4mile@naver.com>
2021-09-07 03:02:01 +0900

    BJ_S5_1010_다리놓기

--- github/src/com/baekJoon/BJ_S5_1010_다리놓기.java
+++ github/src/com/baekJoon/BJ_S5_1010_다리놓기.java
@@ -15,10 +15,31 @@ public class BJ_S5_1010_다리놓기 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		input = new BufferedReader(new StringReader(src));
-		
+		int T = Integer.parseInt(input.readLine());
+		for(int t=0; t<T; t++) {
+			tokens = new StringTokenizer(input.readLine());
+			int N = Integer.parseInt(tokens.nextToken());
+			int M = Integer.parseInt(tokens.nextToken());
+			answer = 0;
+			combi(0, 0, N, M);
+			System.out.println(answer);
+		}
+	}
+	static int answer;
+	private static void combi(int start, int cnt, int N, int M) {
+		if(cnt == N) {
+			answer++;
+			return;
+		}
+		for(int i=start; i<M; i++) {
+			combi(i, cnt+1, N, M);
+		}
 	}
 
 	static String src =
 
-			"";
+			"3\r\n" + 
+			"2 2\r\n" + 
+			"1 5\r\n" + 
+			"13 29";
 }
