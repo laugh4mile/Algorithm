7739e8815ba29e2ee2d3e8cb90a6b2d35fe4caf2
Hyunsoo <laugh4mile@naver.com>
2021-09-06 23:45:58 +0900

    BJ_S5_1010_다리놓기

---
+++ github/src/com/baekJoon/BJ_S5_1010_다리놓기.java
@@ -0,0 +1,24 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class BJ_S5_1010_다리놓기 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer tokens;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		input = new BufferedReader(new StringReader(src));
+		
+	}
+
+	static String src =
+
+			"";
+}
