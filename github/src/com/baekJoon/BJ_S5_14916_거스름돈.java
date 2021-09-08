9f74f218df6eaa9a9107954756b886a04b5b145b
Hyunsoo <laugh4mile@naver.com>
2021-09-08 23:58:13 +0900

    BJ_S5_14916_거스름돈

--- github/src/com/baekJoon/BJ_S5_14916_거스름돈.java
+++ github/src/com/baekJoon/BJ_S5_14916_거스름돈.java
@@ -5,20 +5,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 
-public class BJ_S5_9655_돌게임 {
-	
+public class BJ_S5_14916_거스름돈 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		input = new BufferedReader(new StringReader(src));
-		int N = Integer.parseInt(input.readLine());
-		if(N%2 == 0) {
-			System.out.println("CY");
-		}else {
-			System.out.println("SK");
+		int n = Integer.parseInt(input.readLine());
+		int dp[] = new int[100001];
+		dp[1] = -1;
+		dp[2] = 1;
+		dp[3] = -1;
+		dp[4] = 2;
+		for(int i=5; i<100001; i++) {
+			if(i%5 == 0) {
+				dp[i] = i/5;
+			}else {
+				dp[i] = dp[i-2]+1;
+			}
 		}
+		System.out.println(dp[n]);
 	}
 
 	static String src =
-			"1";
+
+			"14";
 }
