bf7cb8983e9f3182251c16c0accc820aa3a12652
Hyunsoo <laugh4mile@naver.com>
2021-09-10 23:50:19 +0900

    BJ_S2_15988_123더하기3

---
+++ github/src/com/baekJoon/BJ_S2_15988_123더하기3.java
@@ -0,0 +1,26 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class BJ_S2_15988_123더하기3 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer tokens;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		input = new BufferedReader(new StringReader(src));
+		
+	}
+
+	static String src =
+			"3\r\n" + 
+			"4\r\n" + 
+			"7\r\n" + 
+			"10";
+}
