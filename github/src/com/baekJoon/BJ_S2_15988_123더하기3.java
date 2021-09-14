30c6e9000c9661f04612424a0f9bc82eaa21ef31
Hyunsoo <laugh4mile@naver.com>
2021-09-14 20:03:08 +0900

    BJ_S2_15988_123더하기3 overflow 문제 해결

--- github/src/com/baekJoon/BJ_S2_15988_123더하기3.java
+++ github/src/com/baekJoon/BJ_S2_15988_123더하기3.java
@@ -1,12 +1,9 @@
 package com.baekJoon;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.StringReader;
-import java.util.StringTokenizer;
 
 public class BJ_S2_15988_123더하기3 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -19,17 +16,17 @@ public class BJ_S2_15988_123더하기3 {
 		dp[2] = 2;
 		dp[3] = 4;
 		for(int i=4; i<1000001; i++) {
-			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+			dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]);
+		}
+		for(int i=0; i<1000001; i++) {
+			System.out.println(dp[i]);
 		}
 		StringBuilder sb = new StringBuilder(); 
 		for(int t=0; t<T; t++) {
 			int n = Integer.parseInt(input.readLine());
-			sb.append(dp[n]%1000000009+"\n");
+			sb.append(dp[n]+"\n");
 		}
 		System.out.println(sb);
-//		System.out.println();
-//		System.out.println();
-//		System.out.println(dp[1000000]%1000000009);
 	}
 
 	static String src =
