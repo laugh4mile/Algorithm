ae0dd27e9272ff8fc8791ca755fe23bd3589eae4
Hyunsoo <laugh4mile@naver.com>
2021-09-18 23:45:47 +0900

    BJ_S1_2133_타일채우기 + 이항계수

--- github/src/com/baekJoon/BJ_S1_11051_이항계수2.java
+++ github/src/com/baekJoon/BJ_S1_11051_이항계수2.java
@@ -1,16 +1,13 @@
 package com.baekJoon;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.StringReader;
 import java.util.StringTokenizer;
 
 public class BJ_S1_11051_이항계수2 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer tokens;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
--- github/src/com/baekJoon/BJ_S1_2133_타일채우기.java
+++ github/src/com/baekJoon/BJ_S1_2133_타일채우기.java
@@ -1,5 +1,19 @@
 package com.baekJoon;
 
-public class BJ_S1_2133_타일채우기 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
 
+public class BJ_S1_2133_타일채우기 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		input = new BufferedReader(new StringReader(src));
+		int N = Integer.parseInt(input.readLine());
+		int[] dp = new int[3*N+1];
+	}
+
+	static String src =
+			"2";
 }
