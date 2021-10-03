98b6b4439e096082dd4f355e541a319b5dac95f5
Hyunsoo <laugh4mile@naver.com>
2021-10-04 00:15:20 +0900

    nhn 어렵네;;

---
+++ github/src/com/codingtest/nhn/Q1.java
@@ -0,0 +1,21 @@
+package com.codingtest.nhn;
+
+public class Q1 {
+	public static void main(String[] args) {
+		
+	}
+	static class Node{
+		int a;
+		int c;
+		public Node(int a, int c) {
+			super();
+			this.a = a;
+			this.c = c;
+		}
+		@Override
+		public String toString() {
+			return "Node [a=" + a + ", c=" + c + "]";
+		}
+		
+	}
+}
