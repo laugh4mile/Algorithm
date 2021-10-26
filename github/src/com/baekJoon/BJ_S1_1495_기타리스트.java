72228b715c0079fcc5bf05c6bc3c4363953f7416
Hyunsoo <laugh4mile@naver.com>
2021-10-26 22:23:28 +0900

    BJ_S1_1495_기타리스트

---
+++ github/src/com/baekJoon/BJ_S1_1495_기타리스트.java
@@ -0,0 +1,31 @@
+package com.baekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class BJ_S1_1495_기타리스트 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer tokens;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		input = new BufferedReader(new StringReader(src));
+		tokens = new StringTokenizer(input.readLine());
+		int N = Integer.parseInt(tokens.nextToken());
+		int S = Integer.parseInt(tokens.nextToken());
+		int M = Integer.parseInt(tokens.nextToken());
+		for(int i=0; i<N; i++) {
+			
+		}
+	}
+
+	static String src =
+
+			"3 5 10\r\n" + 
+			"5 3 7";
+}
