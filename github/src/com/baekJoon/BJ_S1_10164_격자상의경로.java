417a549fa6b935e111534a183ace17d1bf6fb5e2
Hyunsoo <laugh4mile@naver.com>
2021-11-01 00:21:40 +0900

    BJ_S1_10164_격자상의경로

--- github/src/com/baekJoon/BJ_S1_10164_격자상의경로.java
+++ github/src/com/baekJoon/BJ_S1_10164_격자상의경로.java
@@ -1,25 +1,28 @@
 package com.baekJoon;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.StringReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BJ_S1_10164_격자상의경로 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer tokens;
-
+	static int N,M,K;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		input = new BufferedReader(new StringReader(src));
 		tokens = new StringTokenizer(input.readLine());
-		int N = Integer.parseInt(tokens.nextToken());
-		int M = Integer.parseInt(tokens.nextToken());
-		int K = Integer.parseInt(tokens.nextToken());
+		N = Integer.parseInt(tokens.nextToken());
+		M = Integer.parseInt(tokens.nextToken());
+		K = Integer.parseInt(tokens.nextToken());
+		
+		int answer = solution();
+		System.out.println(answer);
+		
+	}
+
+	private static int solution() {
 		int dp[][] = new int[N][M];
 		for(int i=0; i<N; i++) {
 			dp[i][0] = 1;
@@ -27,24 +30,30 @@ public class BJ_S1_10164_격자상의경로 {
 		for(int i=0; i<M; i++) {
 			dp[0][i] = 1;
 		}
-		outer : for(int r=1; r<N; r++) {
+		for(int r=1; r<N; r++) {
 			for(int c=1; c<M; c++) {
 				dp[r][c] = dp[r-1][c] + dp[r][c-1];
-				if(M*r+c+1==K) break outer;
 			}	
 		}
-		System.out.println(K/M+" "+K%M);
-		for(int r=K/M; r<N; r++) {
-			for(int c=K%M-1; c<M; c++) {
-				
+		if (K==0) return dp[N-1][M-1]; // 0이면 마지막 인덱스 리턴
+		K--;
+		int nr = K/M;
+		int nc = K%M;
+		for(int i=nr; i<N; i++) {
+			dp[i][nc] = dp[nr][nc];
+		}
+		for(int i=nc; i<M; i++) {
+			dp[nr][i] = dp[nr][nc];
+		}
+		for(int r=nr+1; r<N; r++) {
+			for(int c=nc+1; c<M; c++) {
+				dp[r][c] = dp[r-1][c] + dp[r][c-1];
 			}	
 		}
-		
-		for(int x[]: dp) {
-			System.out.println(Arrays.toString(x));
-		}
+
+		return dp[N-1][M-1];
 	}
 
 	static String src =
-			"3 5 8";
+			"1 1 0";
 }
