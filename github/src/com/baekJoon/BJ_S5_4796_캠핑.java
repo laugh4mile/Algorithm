858bf62a6c2056a1f2feef315830eef8f7938d8d
Hyunsoo <laugh4mile@naver.com>
2021-11-07 20:51:30 +0900

    BJ_S5_4796_캠핑

--- github/src/com/baekJoon/BJ_S5_4796_캠핑.java
+++ github/src/com/baekJoon/BJ_S5_4796_캠핑.java
@@ -1,16 +1,40 @@
 package com.baekJoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.StringReader;
+import java.util.StringTokenizer;
 
 public class BJ_S5_4796_캠핑 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static int L, P, V;
-	public static void main(String[] args) {
+	static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws Exception {
 		input = new BufferedReader(new StringReader(src));
-		
+		StringTokenizer tokens;
+		int t = 1;
+		while(true) {
+			tokens = new StringTokenizer(input.readLine());
+			int L = Integer.parseInt(tokens.nextToken());
+			int P = Integer.parseInt(tokens.nextToken());
+			int V = Integer.parseInt(tokens.nextToken());
+			if(L == 0) {
+				break;
+			}
+			int answer = 0;
+			answer += V/P*L;
+			if(V%P > L) {
+				answer += L;
+			}else {
+				answer += V%P;
+			}
+			output.append("Case "+ t++ +": "+answer+"\n");
+		}
+		output.close();
 	}
+	
 	static String src = 
 			"5 8 20\r\n" + 
 			"5 8 17\r\n" + 
