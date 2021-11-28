4a8ec44b7d7726cd9f3874ae33257b29d6328d45
Hyunsoo <laugh4mile@naver.com>
2021-11-28 16:16:41 +0900

    BJ_G5_1756_피자굽기. 시간초과를 해결할 방법 : oven을 정렬된 상태로 받는다.

--- github/src/com/baekJoon/BJ_G5_1756_피자굽기_2.java
+++ github/src/com/baekJoon/BJ_G5_1756_피자굽기_2.java
@@ -3,6 +3,8 @@ package com.baekJoon;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BJ_G5_1756_피자굽기_2 {
@@ -16,31 +18,36 @@ public class BJ_G5_1756_피자굽기_2 {
         int N = Integer.parseInt(tokens.nextToken());
 
         int oven[] = new int[D];
-        tokens = new StringTokenizer(input.readLine());
-        for(int d=0; d<D; d++){
-            oven[d] = Integer.parseInt(tokens.nextToken());
-        }
 
         tokens = new StringTokenizer(input.readLine());
-        for(int n=0; n<N; n++){
-            int pizzaSize = Integer.parseInt(tokens.nextToken());
-            for(int i=0; i<D; i++){
-                if(pizzaSize > oven[i]){
-                    if(i>0){
-                        oven[i-1] = 0;
-                        break;
-                    }else{
-                        System.out.println(0);
-                        return;
-                    }
-                }
+        oven[0] = Integer.parseInt(tokens.nextToken());
+        for(int d=1; d<D; d++){
+            int ovenSize = Integer.parseInt(tokens.nextToken());
+            if(oven[d-1] < ovenSize){ // 아래층의 지름이 위층의 지름보다 커질 필요가 없다.
+                oven[d] = oven[d-1];
+            }else{
+                oven[d] = ovenSize;
             }
         }
-        for (int i=0; i<D; i++){
-            if(oven[i] == 0){
-                System.out.println(i+1);
-                return;
+        // oven은 내림차순으로 정렬이 된 상태이다.
+
+        tokens = new StringTokenizer(input.readLine());
+        Queue<Integer> pizza = new LinkedList<>();
+        for(int n=0; n<N; n++){
+            pizza.offer(Integer.parseInt(tokens.nextToken()));
+        }
+
+        int answer = -1;
+        for(int i=D-1; i>=0; i--){
+            if(!pizza.isEmpty() && oven[i] >= pizza.peek()){
+                pizza.poll();
+                answer = i;
             }
+        }
+        if(!pizza.isEmpty()){
+            System.out.println(0);
+        }else{
+            System.out.println(answer+1);
         }
     }
 
