6b569b1290619ffc9061391730d2db50c2bb59df
Hyunsoo <laugh4mile@naver.com>
2021-12-05 23:54:10 +0900

    BJ_S5_17266_어두운굴다리. 왜 안되지..

---
+++ github/src/com/baekJoon/BJ_S5_17266_어두운굴다리.java
@@ -0,0 +1,40 @@
+package com.baekJoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BJ_S5_17266_어두운굴다리 {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+
+    public static void main(String[] args) throws Exception{
+        input = new BufferedReader(new StringReader(src));
+        int N = Integer.parseInt(input.readLine());
+        int M = Integer.parseInt(input.readLine());
+
+        int arr[] = new int[M];
+        tokens = new StringTokenizer(input.readLine());
+        for(int i=0; i<M; i++){
+            arr[i] = Integer.parseInt(tokens.nextToken());
+        }
+        if(N == 1){
+            System.out.println(1);
+            return;
+        }
+        int distance = arr[0];
+        if(N>1){
+            distance = Math.max(distance, N-arr[M-1]);
+        }
+
+        for(int i=2; i<M; i++){
+            distance = Math.max(distance, (arr[i] - arr[i-1])/2);
+        }
+
+        System.out.println(distance);
+    }
+
+    static String src =
+            "5\n" +
+                    "2\n" +
+                    "0 1";
+}
