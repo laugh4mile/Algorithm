a90cedf5a1cd5669b53b96a898cd5d64f72c3129
laugh4mile <laugh4mile@naver.com>
2022-03-08 14:17:06 +0900

    BJ_G3_14442_벽부수고이동하기2

--- github/src/com/baekJoon/BJ_G3_14442_벽부수고이동하기2.java
+++ github/src/com/baekJoon/BJ_G3_14442_벽부수고이동하기2.java
@@ -4,16 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BJ_G3_14442_벽부수고이동하기2 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer tokens;
-	static int N,M,K,map[][];
+	static int N,M,K,map[][],distance[][];
 	static boolean isVisited[][][];
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		input = new BufferedReader(new StringReader(src));
 		tokens = new StringTokenizer(input.readLine());
@@ -21,67 +19,73 @@ public class BJ_G3_14442_벽부수고이동하기2 {
 		M = Integer.parseInt(tokens.nextToken());
 		K = Integer.parseInt(tokens.nextToken());
 		map = new int[N][M];
-		isVisited = new boolean[N][M][K+1];
+		distance = new int[N][M];
+		isVisited = new boolean[N][M][K+1]; // 0개 ~ K개
 		for(int r=0; r<N; r++) {
 			String line = input.readLine();
 			for(int c=0; c<M; c++) {
 				map[r][c] = line.charAt(c) - '0';
-			}	
-		}
-		bfs();
-	}
-
-	private static void bfs() {
-		Queue<Node> queue = new LinkedList<>();
-		queue.offer(new Node(0, 0, 0, 0));
-		isVisited[0][0][0] = true;
-		
-		while(!queue.isEmpty()) {
-			Node front = queue.poll();
-			
-			if(front.r == N-1 && front.c == M-1) {
-				System.out.println(front.t+1);
-				return;
+				distance[r][c] = Integer.MAX_VALUE;
 			}
-			
-			for(int d=0; d<4; d++) {
+		}
+		bfs(0,0);
+		if(distance[N-1][M-1] == Integer.MAX_VALUE){
+			System.out.println(-1);
+		}else{
+			System.out.println(distance[N-1][M-1]);
+		}
+	}
+	static void bfs(int r, int c){
+		Queue<Node> queue = new LinkedList<>();
+		queue.offer(new Node(r,c,0,1));
+		distance[r][c] = 1;
+		isVisited[r][c][0] = true;
+
+		while(!queue.isEmpty()){
+			Node front = queue.poll();
+			if(distance[front.r][front.c] > front.d){
+				distance[front.r][front.c] = front.d;
+			}
+//			System.out.println(front);
+			for(int d=0; d<4; d++){
 				int nr = front.r + dr[d];
 				int nc = front.c + dc[d];
-				int nk = front.k;
-				int nt = front.t + 1;
-				if(isIn(nr, nc) && !isVisited[nr][nc][nk]&& map[nr][nc] == 0) {
-					isVisited[nr][nc][nk] = true;
-					queue.offer(new Node(nr, nc, nk, nt));
-				}
-				if(front.k != K) {
-					nk = Math.min(front.k+1, K);
-					
-					if(isIn(nr, nc) && !isVisited[nr][nc][nk] && map[nr][nc] == 1) {
-						isVisited[nr][nc][nk] = true;
-						queue.offer(new Node(nr, nc, nk, nt));
+
+				if(isIn(nr,nc) && !isVisited[nr][nc][front.v]){
+					isVisited[nr][nc][front.v] = true;
+					if(map[nr][nc] == 0){
+						queue.offer(new Node(nr,nc,front.v,front.d+1));
+					}else{
+						if(front.v < K){
+							queue.offer(new Node(nr,nc,front.v+1, front.d+1));
+						}
 					}
 				}
 			}
+
 		}
-		System.out.println(-1);
-		return;
-		
 	}
-	static class Node{
+
+	static class Node {
 		int r;
 		int c;
-		int k;
-		int t;
-		public Node(int r, int c, int k, int t) {
-			super();
+		int v; // 부순 벽의 개수
+		int d; // 현재 이동거리
+
+		public Node(int r, int c, int v, int d) {
 			this.r = r;
 			this.c = c;
-			this.k = k;
-			this.t = t;
+			this.v = v;
+			this.d = d;
 		}
 		@Override
 		public String toString() {
-			return "Node [r=" + r + ", c=" + c + ", k=" + k + ", t=" + t + "]";
+			return "Node{" +
+					"r=" + r +
+					", c=" + c +
+					", v=" + v +
+					", d=" + d +
+					'}';
 		}
 	}
 	
@@ -92,11 +96,9 @@ public class BJ_G3_14442_벽부수고이동하기2 {
 		return (r>=0 && c>=0 && r<N && c<M);
 	}
 	static String src =
-			"6 4 1\r\n" + 
-			"0100\r\n" + 
-			"1110\r\n" + 
-			"1000\r\n" + 
-			"0000\r\n" + 
-			"0111\r\n" + 
-			"0000";
+			"4 4 3\n" +
+					"0111\n" +
+					"1111\n" +
+					"1111\n" +
+					"1110";
 }
