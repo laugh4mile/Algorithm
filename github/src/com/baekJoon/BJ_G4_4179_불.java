d94659944e203738e68e397a637919d7aec56dca
Hyunsoo <laugh4mile@naver.com>
2022-03-24 23:39:29 +0900

    BJ_G4_4179_불 메모리초과

--- github/src/com/baekJoon/BJ_G4_4179_불.java
+++ github/src/com/baekJoon/BJ_G4_4179_불.java
@@ -1,7 +1,6 @@
 package com.baekJoon;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -12,7 +11,7 @@ public class BJ_G4_4179_불 {
     static StringTokenizer tokens;
     static int R, C;
     static char map[][];
-    static boolean isVisited[][];
+    static boolean isVisited[][][];
 
     public static void main(String[] args) throws IOException {
         input = new BufferedReader(new StringReader(src));
@@ -20,7 +19,7 @@ public class BJ_G4_4179_불 {
         R = Integer.parseInt(tokens.nextToken());
         C = Integer.parseInt(tokens.nextToken());
         map = new char[R][C];
-        isVisited = new boolean[R][C];
+        isVisited = new boolean[2][R][C];
 
         int startR = 0;
         int startC = 0;
@@ -34,49 +33,57 @@ public class BJ_G4_4179_불 {
                 if(map[r][c] == 'J'){
                     startR = r;
                     startC = c;
-//                    map[r][c] = '#';
                 }
                 if(map[r][c] == 'F'){
                     fireR = r;
                     fireC = c;
-//                    map[r][c] = '.';
                 }
             }
         }
 
         escape(startR, startC, fireR, fireC);
-
-        for(char[] x : map){
-            System.out.println(Arrays.toString(x));
-        }
-
     }
 
     private static void escape(int startR, int startC, int fireR, int fireC) {
         Queue<Node> queue = new LinkedList<>();
         queue.offer(new Node(startR,startC, 'J', 1)); // 지훈이를 queue에 먼저 넣어야 함!
         queue.offer(new Node(fireR,fireC, 'F', 1));
-        isVisited[startR][startC] = true;
-        isVisited[fireR][fireC] = true;
+        isVisited[0][startR][startC] = true;
+        isVisited[1][fireR][fireC] = true;
 
+        int nr = 0;
+        int nc = 0;
+        Node front;
         while (!queue.isEmpty()) {
-            Node front = queue.poll();
-            System.out.println(front);
+            front = queue.poll();
+            if(front.JorF == 'J'){
+                isVisited[0][front.r][front.c] = true;
+            }else isVisited[1][front.r][front.c] = true;
             if((front.r == 0 || front.r == R-1 || front.c == 0 || front.c == C-1) && front.JorF == 'J'){
-
+                boolean flag = false;
+                for(int d=0; d<4; d++){
+                    nr = front.r + dr[d];
+                    nc = front.c + dc[d];
+                    if(isIn(nr,nc) && isVisited[1][nr][nc]){
+                        flag = true;
+                        break;
+                    }
+                }
+                if(!flag){
+                    System.out.println(front.time);
+                    return;
+                }
             }
 
             for(int d=0; d<4; d++){
-                int nr = front.r + dr[d];
-                int nc = front.c + dc[d];
+                nr = front.r + dr[d];
+                nc = front.c + dc[d];
                 if(front.JorF == 'J'){
-                    if(isIn(nr,nc) && !isVisited[nr][nc] && map[nr][nc] == '.'){
-                        isVisited[nr][nc] = true;
+                    if(isIn(nr,nc) && !isVisited[0][nr][nc] && !isVisited[1][nr][nc] && map[nr][nc] == '.'){
                         queue.offer(new Node(nr,nc,front.JorF,front.time+1));
                     }
                 }else{
-                    if(isIn(nr,nc) && !isVisited[nr][nc] && map[nr][nc] != '#'){ /////////////// isVisited[][]를 isVisited[][][] 로 해야할듯?
-                        isVisited[nr][nc] = true;
+                    if(isIn(nr,nc) && !isVisited[1][nr][nc] && map[nr][nc] != '#'){
                         queue.offer(new Node(nr,nc,front.JorF,front.time+1));
                     }
                 }
@@ -98,16 +105,6 @@ public class BJ_G4_4179_불 {
             this.JorF = JorF;
             this.time = time;
         }
-
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "r=" + r +
-                    ", c=" + c +
-                    ", JorF=" + JorF +
-                    ", time=" + time +
-                    '}';
-        }
     }
 
     static int dr[] = {0,0,-1,1};
@@ -118,9 +115,9 @@ public class BJ_G4_4179_불 {
     }
 
     static String src =
-            "4 4\n" +
-            "####\n" +
-            "#JF#\n" +
-            "#..#\n" +
-            "#...";
+            "4 5\n" +
+            "#####\n" +
+            "#J..#\n" +
+            "#...#\n" +
+            "#...F";
 }
