a287ff5b223966e976eb71aba2bab4abde88e804
laugh4mile <laugh4mile@naver.com>
2022-03-28 18:00:19 +0900

    BJ_S2_1182_부분수열의합 진행중

---
+++ github/src/com/baekJoon/BJ_S2_1182_부분수열의합.java
@@ -0,0 +1,25 @@
+package com.baekJoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BJ_S2_1182_부분수열의합 {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer tokens;
+
+    public static void main(String[] args) throws IOException {
+        input = new BufferedReader(new StringReader(src));
+        tokens = new StringTokenizer(input.readLine());
+        int N = Integer.parseInt(tokens.nextToken());
+        int S = Integer.parseInt(tokens.nextToken());
+
+        for(int i=0; i<N; i++){
+
+        }
+    }
+
+    static String src =
+            "5 0\n" +
+            "-7 -3 -2 5 8";
+}
