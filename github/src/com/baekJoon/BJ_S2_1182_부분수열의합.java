de3aab042849a4cd49cb0338f12c3c890016c4ff
laugh4mile <laugh4mile@naver.com>
2022-03-29 17:30:03 +0900

    BJ_S2_1182_부분수열의합 해결

--- github/src/com/baekJoon/BJ_S2_1182_부분수열의합.java
+++ github/src/com/baekJoon/BJ_S2_1182_부분수열의합.java
@@ -7,16 +7,34 @@ public class BJ_S2_1182_부분수열의합 {
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer tokens;
-
+    static int N, S, arr[], answer;
     public static void main(String[] args) throws IOException {
         input = new BufferedReader(new StringReader(src));
         tokens = new StringTokenizer(input.readLine());
-        int N = Integer.parseInt(tokens.nextToken());
-        int S = Integer.parseInt(tokens.nextToken());
-
+        N = Integer.parseInt(tokens.nextToken());
+        S = Integer.parseInt(tokens.nextToken());
+        arr = new int[N];
+        tokens = new StringTokenizer(input.readLine());
         for(int i=0; i<N; i++){
-
+            arr[i] = Integer.parseInt(tokens.nextToken());
         }
+        dfs(0,0);
+        if(S == 0){
+            System.out.println(answer-1);
+        }else{
+            System.out.println(answer);
+        }
+    }
+
+    private static void dfs(int i, int sum) {
+        if(i == N) {
+            if(sum == S) {
+                answer++;
+            }
+            return;
+        }
+        dfs(i+1, sum); // 선택안할경우
+        dfs(i+1, sum + arr[i]); // 선택할경우
     }
 
     static String src =
