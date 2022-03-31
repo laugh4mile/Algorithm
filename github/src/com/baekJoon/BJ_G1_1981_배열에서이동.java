47ce4e267f8ae0c917115fcaad0598ca49be010c
laugh4mile <laugh4mile@naver.com>
2022-03-31 17:55:09 +0900

    BJ_G1_1981_배열에서이동 외않됨?

--- github/src/com/baekJoon/BJ_G1_1981_배열에서이동.java
+++ github/src/com/baekJoon/BJ_G1_1981_배열에서이동.java
@@ -1,9 +1,7 @@
 package com.baekJoon;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BJ_G1_1981_배열에서이동 {
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -16,53 +14,41 @@ public class BJ_G1_1981_배열에서이동 {
         N = Integer.parseInt(input.readLine());
         map = new int[N][N];
         isVisited = new boolean[N][N];
-        int left = Integer.MAX_VALUE;
+        int left = 0;
         int right = 0;
         for(int r=0; r<N; r++){
             tokens = new StringTokenizer(input.readLine());
             for(int c=0; c<N; c++){
                 map[r][c] = Integer.parseInt(tokens.nextToken());
-                if(map[r][c] < left){
-                    left = map[r][c];
-                }
                 if(map[r][c] > right){
                     right = map[r][c];
                 }
             }
         }
-
+        int answer = 0;
         int mid = 0;
-        while(left < right){
-            System.out.println(left +"  " + right);
-            mid = (right - left) / 2;
-
+        while(left <= right){
+            mid = (right + left) / 2;
             if(bfs(mid)){
                 right = mid-1;
+                answer = mid;
             }else{
                 left = mid+1;
             }
         }
-
+        System.out.println(answer);
     }
 
     private static boolean bfs(int mid) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(0,0, Integer.MAX_VALUE, 0));
+        isVisited = new boolean[N][N];
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        queue.offer(new Node(0,0, Math.min(map[0][0], map[N-1][N-1]), Math.max(map[0][0], map[N-1][N-1])));
         isVisited[0][0] = true;
+
         while(!queue.isEmpty()){
             Node front = queue.poll();
-            if(map[front.r][front.c] > front.max){
-                front.max = map[front.r][front.c];
-            }
-            if(map[front.r][front.c] < front.min){
-                front.min = map[front.r][front.c];
-            }
-            if(front.max - front.min <= mid){
-                if(front.r == N-1 && front.c == N-1){
-                    return true;
-                }
-            }else{
-                continue;
+            if(front.r == N-1 && front.c == N-1){
+                return true;
             }
 
             for(int d=0; d<4; d++){
@@ -70,15 +56,25 @@ public class BJ_G1_1981_배열에서이동 {
                 int nc = front.c + dc[d];
 
                 if(isIn(nr,nc) && !isVisited[nr][nc]){
-                    isVisited[nr][nc] = true;
-                    queue.offer(new Node(nr,nc,front.min, front.max));
+                    int max = front.max;
+                    int min = front.min;
+                    if(map[nr][nc] > front.max){
+                        max = map[nr][nc];
+                    }
+                    if(map[nr][nc] < front.min){
+                        min = map[nr][nc];
+                    }
+                    if(max - min <= mid){
+                        isVisited[nr][nc] = true;
+                        queue.offer(new Node(nr,nc,min, max));
+                    }
                 }
             }
         }
         return false;
     }
 
-    static class Node{
+    static class Node implements Comparable<Node>{
         int r;
         int c;
         int min;
@@ -89,6 +85,21 @@ public class BJ_G1_1981_배열에서이동 {
             this.c = c;
             this.min = min;
             this.max = max;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "r=" + r +
+                    ", c=" + c +
+                    ", min=" + min +
+                    ", max=" + max +
+                    '}';
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.max-this.min, o.max-o.min);
         }
     }
 
@@ -101,9 +112,9 @@ public class BJ_G1_1981_배열에서이동 {
 
     static String src =
             "5\n" +
-            "1 1 3 6 8\n" +
-            "1 2 2 5 5\n" +
-            "4 4 0 3 3\n" +
-            "8 0 2 3 4\n" +
-            "4 3 0 2 1";
+                    "1 1 3 6 8\n" +
+                    "1 2 2 5 5\n" +
+                    "4 4 0 3 3\n" +
+                    "8 0 2 3 4\n" +
+                    "4 3 0 2 1";
 }
