1ac2a5afb2d26cde0416d0617c57cb847526c452
laugh4mile <laugh4mile@naver.com>
2023-05-15 10:20:04 +0900

    BJ_G4_2916_자와각도기

--- src/com/baekJoon/BJ_G4_2916_자와각도기.java
+++ src/com/baekJoon/BJ_G4_2916_자와각도기.java
@@ -3,7 +3,7 @@ package com.baekJoon;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class BJ_G4_자와각도기 {
+public class BJ_G4_2916_자와각도기 {
     BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) {
