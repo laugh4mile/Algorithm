5adfbbfa612a780934a61e1d74b481a500ea9856
Hyunsoo <laugh4mile@naver.com>
2023-08-19 15:15:10 +0900

    BJ_G4_12869_뮤탈리스크.java

--- src/com/baekJoon/BJ_G4_12869_뮤탈리스크.java
+++ src/com/baekJoon/BJ_G4_12869_뮤탈리스크.java
@@ -3,14 +3,21 @@ package com.baekJoon;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BJ_G4_12869_뮤탈리스크 {
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer tokens;
-    static int answer = Integer.MAX_VALUE;
-    static int scvs[], size;
+    static int scvs[], size, attack[][] = {
+            {1,3,9},
+            {1,9,3},
+            {3,1,9},
+            {3,9,1},
+            {9,1,3},
+            {9,3,1}
+    };
     static boolean isVisited[][][];
 
     public static void main(String[] args) throws Exception{
@@ -24,27 +31,70 @@ public class BJ_G4_12869_뮤탈리스크 {
         size = 61;
         isVisited = new boolean[size][size][size];
 
-        solve(scvs[0], scvs[1], scvs[2], 0);
-        System.out.println(answer);
+        System.out.println(bfs(scvs[0], scvs[1], scvs[2]));
     }
 
-    private static void solve(int scv1, int scv2, int scv3, int depth) {
-        if(depth > answer) return;
-        if(scv1 <= 0 && scv2 <= 0 && scv3 <= 0){
-            if(depth < answer){
-                answer = depth;
+    private static int bfs(int scv1, int scv2, int scv3) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(scv1, scv2, scv3, 0));
+        visit(scv1, scv2, scv3);
+
+
+        while(!queue.isEmpty()){
+            Node front = queue.poll();
+//            System.out.println(front);
+            if(front.scv1 <= 0 && front.scv2 <= 0 && front.scv3 <= 0){
+                return front.cnt;
             }
-            return;
+            for(int i=0; i<6; i++){
+                int n1 = Math.max(front.scv1 - attack[i][0], 0);
+                int n2 = Math.max(front.scv2 - attack[i][1], 0);
+                int n3 = Math.max(front.scv3 - attack[i][2], 0);
+
+                if(!isVisited[n1][n2][n3]){
+                    visit(n1,n2,n3);
+                    queue.offer(new Node(n1,n2,n3,front.cnt+1));
+                }
+            }
         }
-        solve(scv1-9, scv2-3, scv3-1, depth+1);
-        solve(scv1-9, scv3-3, scv2-1, depth+1);
-        solve(scv2-9, scv1-3, scv3-1, depth+1);
-        solve(scv2-9, scv3-3, scv1-1, depth+1);
-        solve(scv3-9, scv1-1, scv2-1, depth+1);
-        solve(scv3-9, scv2-2, scv1-1, depth+1);
+
+        return 0;
     }
 
+    private static void visit(int scv1, int scv2, int scv3) {
+        isVisited[scv1][scv2][scv3] = true;
+        isVisited[scv1][scv3][scv2] = true;
+        isVisited[scv2][scv1][scv3] = true;
+        isVisited[scv2][scv3][scv1] = true;
+        isVisited[scv3][scv1][scv2] = true;
+        isVisited[scv3][scv2][scv1] = true;
+    }
+
+    private static class Node {
+
+        int scv1;
+        int scv2;
+        int scv3;
+        int cnt;
+        public Node(int scv1, int scv2, int scv3, int cnt) {
+            this.scv1 = scv1;
+            this.scv2 = scv2;
+            this.scv3 = scv3;
+            this.cnt = cnt;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "scv1=" + scv1 +
+                    ", scv2=" + scv2 +
+                    ", scv3=" + scv3 +
+                    ", cnt=" + cnt +
+                    '}';
+        }
+
+    }
     static String src =
             "3\n" +
-            "60 60 60";
+                    "12 10 4";
 }
