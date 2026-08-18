db28b77cdf4f94babfee17003d289db387a1ac57
Hyunsoo <laugh4mile@naver.com>
2026-08-18 15:57:45 +0900

    Algo-holic week22

--- src/com/programmers/L2_멀리뛰기.java
+++ src/com/programmers/L2_멀리뛰기.java
@@ -1,17 +1,20 @@
+// PGM - 12914 L2_멀리뛰기
+// DP
+// https://school.programmers.co.kr/learn/courses/30/lessons/12914?language=java
 package com.programmers;
 
 class L2_멀리뛰기 {
-    public static long solution(int n) {
-        long answer[] = new long[2001];
-        answer[1] = 1;
-        answer[2] = 2;
-        for(int i=3; i<=n; i++){
-            answer[i] = (answer[i-1] + answer[i-2]) % 1234567;
+    public long solution(int n) {
+        long answer = 0;
+        long[] dp = new long[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<n+1; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
         }
-        return answer[n];
-    }
+        // System.out.println(Arrays.toString(dp));
+        answer = dp[n];
 
-    public static void main(String[] args) {
-        System.out.println(solution(5));
+        return answer;
     }
 }
--- src/com/programmers/L3_정수삼각형.java
+++ src/com/programmers/L3_정수삼각형.java
@@ -1,34 +1,32 @@
+// PGM - 43105 L3_정수삼각형
+// DP
+// https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=java
 package com.programmers;
 
-import java.util.Arrays;
-
 class L3_정수삼각형 {
-    public static int solution(int[][] triangle) {
+    public int solution(int[][] triangle) {
         int answer = 0;
-        int dp[][] = new int[triangle.length+1][triangle.length+1];
+        int size = triangle.length;
+        int max[][] = new int[size][size];
+        max[0][0] = triangle[0][0];
 
-        for(int r=0; r<triangle.length; r++){
-            for(int c=0; c<triangle[r].length; c++){
-                dp[r+1][c+1] = triangle[r][c];
+        for(int r=0; r<size-1; r++){
+            for(int c=0; c<r+1; c++){
+                max[r+1][c] = Math.max(max[r+1][c], max[r][c]+triangle[r+1][c]);
+                max[r+1][c+1] = Math.max(max[r+1][c+1], max[r][c]+triangle[r+1][c+1]);
             }
         }
 
-        for (int r=1; r<dp.length; r++){
-            for (int c=1; c<dp.length; c++){
-                dp[r][c] = Math.max(dp[r-1][c], dp[r-1][c-1]) + dp[r][c];
-                answer = Math.max(answer, dp[r][c]);
-            }
-        }
+        // for(int x[] : max){
+        //     System.out.println(Arrays.toString(x));
+        // }
 
-        for(int[] x : dp){
-            System.out.println(Arrays.toString(x));
+        for(int c=0; c<size; c++){
+            if(answer < max[size-1][c]){
+                answer = max[size-1][c];
+            }
         }
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-        int[][] triangle = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
-        System.out.println(solution(triangle));
     }
 }
